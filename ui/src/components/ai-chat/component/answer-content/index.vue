d61ee3095e0d267d2aa7981142c3837d4d926a04
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2026-03-03 11:07:50 +0800

    fix: chat style

--- ui/src/components/ai-chat/component/answer-content/index.vue
+++ ui/src/components/ai-chat/component/answer-content/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="item-content lighter">
-    <el-space :fill="true" wrap>
+    <el-space :fill="true" wrap class="w-full">
       <div v-for="(answer_text, index) in answer_text_list" :key="index">
         <div class="avatar mr-8" v-if="showAvatar">
           <img v-if="application.avatar" :src="application.avatar" height="28px" width="28px" />
