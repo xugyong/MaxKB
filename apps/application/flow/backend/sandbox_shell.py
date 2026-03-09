056ff2794dc7048cda71735646f74dbd8adb3910
CaptainB <bin@fit2cloud.com>
2026-03-09 15:23:27 +0800

    feat: inherit current process environment in SandboxShellBackend

--- apps/application/flow/backend/sandbox_shell.py
+++ apps/application/flow/backend/sandbox_shell.py
@@ -1,4 +1,5 @@
 import getpass
+import os
 
 from deepagents.backends import LocalShellBackend
 from deepagents.backends.protocol import ExecuteResponse
@@ -7,10 +8,17 @@ from maxkb.const import CONFIG
 
 _enable_sandbox = bool(CONFIG.get('SANDBOX', 0))
 _run_user = 'sandbox' if _enable_sandbox else getpass.getuser()
+_sandbox_python_sys_path = CONFIG.get_sandbox_python_package_paths().split(',')
 
 
 class SandboxShellBackend(LocalShellBackend):
     def __init__(self, root_dir: str, **kwargs):
+        if 'env' not in kwargs and not kwargs.get('inherit_env', False):
+            env = os.environ.copy()
+            path = env.get('PATH', '/usr/bin:/bin')
+            if _sandbox_python_sys_path not in path.split(os.pathsep):
+                env['PATH'] = f"{_sandbox_python_sys_path}{os.pathsep}{path}"
+            kwargs['env'] = env
         super().__init__(root_dir=root_dir, **kwargs)
 
     def execute(
