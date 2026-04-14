6e43898f7ea77a58a00594c69b156a323c098a31
xuguoyong <xuguoyong@finogeeks.com>
2026-04-14 18:44:53 +0800

    fix bug

--- apps/maxkb/settings/base/web.py
+++ apps/maxkb/settings/base/web.py
@@ -28,7 +28,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
